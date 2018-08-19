9868323ce81a1c3f3658f71d97fe89aefa85dcaa
Yurina Shin <yurinas@gmail.com>
2018-08-19 12:04:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/newteam' do
+    @team_name = params[:name]
+    @team_array = [params[:coach], params[:pg], params[:sg], params[:pf], params[:sf], params[:c]]
+
+    erb :team
+  end
 
 
 end
